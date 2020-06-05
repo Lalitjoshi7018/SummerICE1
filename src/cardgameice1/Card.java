851531559084b048aceeed49e2cc9ab09f085a06
inderdeep12 <66023957+inderdeep12@users.noreply.github.com>
2020-06-05 18:18:28 -0400

    Update Card.java

--- src/cardgameice1/Card.java
+++ src/cardgameice1/Card.java
@@ -1,4 +1,5 @@
-/*
+/* Student name - Inderdeep Singh
+   Student Id - 991587292
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
