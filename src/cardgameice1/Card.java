7eb10d5b2bd2a73c9b9fd5b8cc6b26896952540e
ZarnaShethwala <shethwal@sheridancollege.ca>
2020-06-05 20:35:39 -0400

    CardgameICE1 completed

--- src/cardgameice1/Card.java
+++ src/cardgameice1/Card.java
@@ -5,41 +5,47 @@
  */
 package cardgameice1;
 
+import java.util.Random; 
+
 /**
  *
  * @author srinivsi
- 
- private variable then refactor it
- * - encapsulate fields (encapsulation principle) 
- getters/setters
+ * @Modifier zarna shethwala Student Number : 991583435
+ *
+ * private variable then refactor it - encapsulate fields (encapsulation
+ * principle) getters/setters
  */
-public class Card 
-{
-  private String suits;//diamonds,clubs,spades, hearts
-  private int value; // 1 to 13
-public static final String[] SUITS={"diamonds","clubs","spades","hearts"};
-    
+public class Card {
+
+    private String suits;//diamonds,clubs,spades, hearts
+    private int value; // 1 to 13
+    public static final String[] SUITS = {"diamonds", "clubs", "spades", "hearts"};
+
     public String getSuits() {
         return suits;
     }
 
-    
     public void setSuits(String suits) {
         this.suits = suits;
     }
 
-    
     public int getValue() {
         return value;
     }
 
-    
     public void setValue(int value) {
         this.value = value;
     }
-//public int randomValue()
-//{
-    //int value= // 
-           // return value;
-//}
+
+    public int createRandomValueSuit() {
+        int randomSuitValue = (int) (Math.random() * 4);
+        return randomSuitValue;
+
+    }
+
+    public int createRandomValueCard() {
+
+        int randonCardvalue = (int) (Math.random() * 13) + 1;
+        return randonCardvalue;
+    }
 }
--- src/cardgameice1/CardGameICE1.java
+++ src/cardgameice1/CardGameICE1.java
@@ -5,33 +5,51 @@
  */
 package cardgameice1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author srinivsi
- print 7 cards (values,suits) and takes input 
- then prints if matching card is found in an array
+ *  print 7 cards (values,suits) and takes input then prints if matching card is found in an
+ * array
  */
-public class CardGameICE1
-{
+public class CardGameICE1 {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
         // code to generate 7 random cards and store in array
         Card[] magicHand = new Card[7];//array of object
-        for (int i=0;i<magicHand.length;i++)
-        {
-           Card c= new Card();//object
-         //  c.setValue(method to generate random value from 1 to 13);
-         //  c.setSuits(method random suits);
-           magicHand[i]=c;//saving object in array
+        for (int i = 0; i < magicHand.length; i++) {
+            Card c = new Card();//object
+            //  c.setValue(method to generate random value from 1 to 13);
+            //  c.setSuits(method random suits);
+            magicHand[i] = c;//saving object in array
+            c.setValue(c.createRandomValueCard());
+            c.setSuits(Card.SUITS[c.createRandomValueSuit()]);
+            System.out.println(magicHand[i].getSuits() + " " + magicHand[i].getValue());
+
         }
-    //take input from user and compare with array 
-   // value=
-       //     suit =
-    
+        //take input from user and compare with array 
+        System.out.println("\nEnter Card value :");
+        int userCardValue = input.nextInt();
+        System.out.println("Enter Suit value :");
+        String userSuitValue = input.next();
+
+        int flag = 0;
+        for (Card magicHand1 : magicHand) {
+            if ((userCardValue == magicHand1.getValue()) && (userSuitValue.equals(magicHand1.getSuits()))) {
+                flag = 1;
+            } 
+        }
+        if (flag == 1) {
+            System.out.println("\nYour card is in the array");
+        } else {
+            System.out.println("\nYour card is not in the array");
+        }
+
     }
-    
 }
