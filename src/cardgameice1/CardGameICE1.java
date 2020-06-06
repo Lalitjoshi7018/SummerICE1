5774b6cf656a8f10ec7e756d82991458ff10cbe4
kaur8700 <kaur8700@sheridancollege.ca>
2020-06-05 21:18:53 -0400

    First Software assignment

--- src/cardgameice1/CardGameICE1.java
+++ src/cardgameice1/CardGameICE1.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 /**
  *
  * @author srinivsi
- *  @modifier Abhishek Abhishek
+ *  @modifier Gursimranpreet kaur
  Student ID - 991587950
  print 7 cards (values,suits) and takes input 
  then prints if matching card is found in an array
@@ -22,29 +22,29 @@ public class CardGameICE1
     public static void main(String[] args) {
         // code to generate 7 random cards and store in array
         Card[] magicHand = new Card[7];//array of object
-        for (int i = 0; i < magicHand.length; i++) {
-            Card c = new Card ();//object
-            c.setValue(c.randomValue(1, 13)); // method to generate random value from 1 to 13
-            c.setSuits(c.SUITS[c.randomValue(0, 3)]); // method random suits generating 0 to 3 for array
-            magicHand[i] = c;     //saving object in array
+        for (int x = 0; x < magicHand.length; x++) {
+            Card xyz = new Card ();//object
+            xyz.setValue(xyz.randomValue(1, 13)); // method to generate random value from 1 to 13
+            xyz.setSuits(xyz.SUITS[xyz.randomValue(0, 3)]); // method random suits generating 0 to 3 for array
+            magicHand[x] = xyz;     //saving object in array
         }
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter any card");
         int value = sc.nextInt();
-        System.out.println("Enter any Suit");
+        System.out.println("Enter the name of any Suit");
         String suit = sc.next();
 
         boolean comparison = false;
 
-        for (int i = 0; i < magicHand.length; i++) {
-            if ((value == magicHand[i].getValue()) && (suit.equals(magicHand[i].getSuits()))) comparison = true;
+        for (int x = 0; x < magicHand.length; x++) {
+            if ((value == magicHand[x].getValue()) && (suit.equals(magicHand[x].getSuits()))) comparison = true;
         }
 
         if (comparison == true) {
-            System.out.println("Your card is in magic hand of Random Cards !!!");
+            System.out.println("Your card is now in magic hand of Random Cards");
         } else
-            System.out.println("Sorry card is not present !!!");
+            System.out.println("Sorry  the card is not present !!!");
     }
 
 }
