e156f38d9bab8a9e0367bc6ae49da0b3099972a1
ZarnaShethwala <65972207+ZarnaShethwala@users.noreply.github.com>
2020-06-05 21:48:13 -0400

    Update CardGameICE1.java

--- src/cardgameice1/CardGameICE1.java
+++ src/cardgameice1/CardGameICE1.java
@@ -10,6 +10,8 @@ import java.util.Scanner;
 /**
  *
  * @author srinivsi
+ *  @Modifier zarna shethwala
+ *  Student Id : 991583435
  *  print 7 cards (values,suits) and takes input then prints if matching card is found in an
  * array
  */
